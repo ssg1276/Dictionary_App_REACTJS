3a639fec2e47014e0adfd2f8e44d11325fa26b49
Sahil Singh Guleria <135002064+ssg1276@users.noreply.github.com>
2024-03-02 11:12:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import React from 'react'
 import Card from './Component/Card'
 
 function App() {
   return (
-    <div className="w-[100vw]  bg-slate-600 flex text-center h-[100vh] justify-center items-center">
+    <div className="w-screen  bg-purple-600 flex text-center h-screen justify-center items-center">
       <Card />
     </div>
   )
